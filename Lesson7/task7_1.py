4b60ea2d996579c93a43071235ef50f25b717830
Irinka <velwei731@gmail.com>
2022-12-15 17:34:40 +0300

    - add solution task7_1

--- Lesson7/task7_1.py
+++ Lesson7/task7_1.py
@@ -4,10 +4,27 @@
 import json
 import csv
 
-with open("client_basa_json_1.json","r") as f:
+
+with open ('client_basa_json_1.json',"r") as f:
     json_f = f.read()
     print(json_f)
 
 
-     with open ("data.csv" ,"w") as csvf:
-         writer =csv.writer(csvf)
+with open("data.csv","w") as csvf:
+    fieldnames = ["Id","Name","Are","Telefon"]
+    writer = csv.DictWriter(csvf,fieldnames= fieldnames)
+    writer.writeheader()
+    writer.writerows([{"Id":'123456', 'Name':'Irina','Are': '35','Telefon':'None'},
+                      {'Id':"234567","Name":" Olga", "Are":"19", "Telefon": "None"},
+    {'Id':"345678","Name":" Elena", "Are":"29", "Telefon": "None"},
+    {'Id':"456789","Name":" Natali" ,"Are":"55", "Telefon": "None"},
+                      {'Id':"567890","Name":" Maria" ,"Are":"33", "Telefon": "None"}])
+
+
+
+
+
+
+
+
+
